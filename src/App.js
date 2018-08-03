2e820961851b45b1a5113138b704d7f5106df9bc
Ryan Dunton <ryandunton1@gmail.com>
2018-08-03 16:11:50 -0400

    making more updates to authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBSRelHJ-GDCuSABuErTksIRcIJMFFI5Mw",
+        authDomain: "authentication-9e2d8.firebaseapp.com",
+        databaseURL: "https://authentication-9e2d8.firebaseio.com",
+        projectId: "authentication-9e2d8",
+        storageBucket: "authentication-9e2d8.appspot.com",
+        messagingSenderId: "730792194583"
+      });
+  }
+
   render() {
-    return(
+    return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App</Text>
+        <LoginForm />
       </View>
-    )
+    );
   }
 }
 
---
+++ src/components/LoginForm.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
+
+class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    }
+  }
+
+  onButtonPress = () => {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess)
+          .catch(this.onLoginFail);
+      });
+  }
+
+  onLoginSuccess = () => {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  onLoginFail = () => {
+    this.setState({ error: 'Authentication Failed', loading: false });
+  }
+
+  renderButton = () => {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    } else {
+      return (
+        <Button onPress={this.onButtonPress}>
+          Log in
+        </Button>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            onChangeText={email => this.setState({ email })}
+            value={ this.state.email }
+            placeholder="user@gmail.com"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Password"
+            onChangeText={password => this.setState({ password })}
+            value={this.state.password}
+            placeholder="password"
+            secureTextEntry
+          />
+        </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
+        <CardSection>
+          {this.renderButton()}
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
+
+export default LoginForm;
